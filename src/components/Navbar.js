75d7fbb7e71cadfe62dfb17fd6b529286234d997
Niels Louwes <niels.louwes@and.digital>
2022-02-07 22:14:27 +0100

    added basic routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Styled = styled.div``;
+
+function Navbar() {
+  return (
+    <Styled>
+      <h2>Navbar component </h2>
+    </Styled>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+/* eslint-disable prettier/prettier */
+import { render } from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Expenses from './pages/Expenses';
+import Invoices from './pages/Invoices';
+import Products from './pages/Products';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="products" element={<Products />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
